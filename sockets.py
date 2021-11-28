a634a4e97a287f2da5b6b97ca3ea402e59ed8201
Warren <wstix@ualberta.ca>
2021-11-27 17:57:57 -0700

    freetests.py passing

--- sockets.py
+++ sockets.py
@@ -13,8 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from json.decoder import JSONDecodeError
+from typing import List
 import flask
-from flask import Flask, request
+from flask import Flask, request, redirect
 from flask_sockets import Sockets
 import gevent
 from gevent import queue
@@ -59,29 +61,67 @@ class World:
     def world(self):
         return self.space
 
+
 myWorld = World()        
 
-def set_listener( entity, data ):
+
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue()
+
+    def put(self, v):
+        self.queue.put_nowait(v)
+
+    def get(self):
+        return self.queue.get()
+
+
+def set_listener(entity, data):
     ''' do something with the update ! '''
 
+
 myWorld.add_set_listener( set_listener )
+
+clients: List[Client] = list()
         
 @app.route('/')
 def hello():
-    '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return redirect('/static/index.html')
 
-def read_ws(ws,client):
+def read_ws(ws, client):
     '''A greenlet function that reads from the websocket and updates the world'''
-    # XXX: TODO IMPLEMENT ME
-    return None
+    try:
+        while True:
+            msg = ws.receive()
+
+            if msg is not None:
+                packet = json.dumps(json.loads(msg))
+                for client in clients:
+                    client.put(packet)
+            else:
+                break
+    except JSONDecodeError as _:
+        print('Error decoding received message!')
 
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
-    # XXX: TODO IMPLEMENT ME
-    return None
+    client = Client()
+    clients.append(client)
+    g = gevent.spawn(read_ws, ws, client)
+
+    try:
+        while True:
+            msg = client.get()
+            ws.send(msg)
+    except Exception as e:
+        print('Websocket Error:', e)
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
+
+    return {}
 
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
@@ -99,24 +139,28 @@ def flask_post_json():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    data = flask_post_json()[entity]
+    myWorld.set(entity, data)
+
+    return { entity: myWorld.get(entity) }
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
-    '''you should probably return the world here'''
-    return None
+    return myWorld.world()
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
-    '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    data = myWorld.get(entity)
+
+    return data
 
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
 
+    return dict()
 
 
 if __name__ == "__main__":
